506ca717b74702081650e44dc164014202cdb616
Ulifly <ulifly@hotmail.com>
2024-10-31 16:45:07 -0600

    socket.io conections working

--- src/server.mjs
+++ src/server.mjs
@@ -1,24 +1,27 @@
-import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
+import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
 
-import { deckCreation }  from './Game.mjs'
+import { deckCreation }  from './gamelogic.mjs'
 
 const deck = deckCreation();
 console.log(deck)
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+//-----------------
 const PORT = 3000;
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -31,6 +34,6 @@ io.on('connection', (socket) => {
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
